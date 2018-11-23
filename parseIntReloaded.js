17709d3c994547510f10b46c7b6ddf5a7081605e
Daniel Epstein <d.epstein92@gmail.com>
2018-11-23 11:39:03 -0500

    added additional test to parseIntReloaded

--- parseIntReloaded.js
+++ parseIntReloaded.js
@@ -1,11 +1,12 @@
 function parseIntReloaded(string) {
     let numbers = [
-      "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
+      "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
       "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "eighteen", "nineteen", "twenty",
-      "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"
-  ];
+      "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    ];
 
-    let wordArr = string.split(" "),
+    let removeSpecialChar = string.replace(/[^a-zA-Z ]/g, " "),
+        wordArr = removeSpecialChar.split(" "),
         numArray = new Array();
 
     let getValues = ( wordArr,
@@ -43,41 +44,59 @@ function parseIntReloaded(string) {
             if (wordArr[i] === "one") {
                 if (wordArr[i + 1] === "hundred") {
                     if (wordArr[i + 2] === "thousand") {
-                        return 100000;
+                        numArray.push(100000);
                     } else {
-                        return 100;
+                        numArray.push(100);
                     }
                 } else if (wordArr[i + 1] === "thousand") {
-                        return 1000;
+                        numArray.push(1000);
                 } else if (wordArr[i + 1] === "million"){
-                        return 1000000;
+                        numArray.push(1000000);
                 } else{
-                        return 1;
+                        numArray.push(1);
                 }
             } else if(wordArr[i].includes("tw")){
-                return getValues(wordArr, i, "two", "twelve", 2, 20, 12, 200, 2000, 200000);
+                getValues(wordArr, i, "two", "twelve", 2, 20, 12, 200, 2000, 200000);
             } else if(wordArr[i].includes("th")){
-                return getValues(wordArr, i, "three", "thirteen", 3, 30, 13, 300, 3000, 300000);
+                getValues(wordArr, i, "three", "thirteen", 3, 30, 13, 300, 3000, 300000);
             } else if(wordArr[i].includes("fo")){
-                return getValues(wordArr, i, "four", "fourteen", 4, 40, 14, 400, 4000, 400000);
+                getValues(wordArr, i, "four", "fourteen", 4, 40, 14, 400, 4000, 400000);
             } else if(wordArr[i].includes("fi")){
-                return getValues(wordArr, i, "five", "fifteen", 5, 50, 15, 500, 5000, 500000);
+                getValues(wordArr, i, "five", "fifteen", 5, 50, 15, 500, 5000, 500000);
             } else if(wordArr[i].includes("six")){
-                return getValues(wordArr, i, "six", "sixteen", 6, 60, 16, 600, 6000, 600000);
+                getValues(wordArr, i, "six", "sixteen", 6, 60, 16, 600, 6000, 600000);
             } else if(wordArr[i].includes("sev")){
-                return getValues(wordArr, i, "seven", "seventeen", 7, 70, 17, 700, 7000, 700000);
+                getValues(wordArr, i, "seven", "seventeen", 7, 70, 17, 700, 7000, 700000);
             } else if(wordArr[i].includes("eig")){
-                return getValues(wordArr, i, "eight", "eighteen", 8, 80, 18, 800, 8000, 800000);
+                getValues(wordArr, i, "eight", "eighteen", 8, 80, 18, 800, 8000, 800000);
             } else if(wordArr[i].includes("nin")){
-                return getValues(wordArr, i, "nine", "nineteen", 9, 90, 19, 900, 9000, 900000);
-            } else if(wordArr[i].includes("sev")){
-                return getValues(wordArr, i, "seven", "seventeen", 7, 70, 17, 700, 7000, 700000);
+                getValues(wordArr, i, "nine", "nineteen", 9, 90, 19, 900, 9000, 900000);
+            } else if(wordArr[i].includes("zero")){
+                numArray.push(0);
+            } else if(wordArr[i].includes("ten")){
+                 if (wordArr[i + 1] === "hundred") {
+                     numArray.push(10000);
+                 } else if (wordArr[i + 1] === "thousand") {
+                     numArray.push(10000);
+                 } else{
+                     numArray.push(10);
+              }
+            } else if(wordArr[i].includes("eleven")){
+                 if (wordArr[i + 1] === "hundred") {
+                    numArray.push(1100);
+                 } else if (wordArr[i + 1] === "thousand") {
+                   numArray.push(11000);
+                 } else{
+                   numArray.push(11);
+                 }
+            } else{
+              numArray.push('Not a number or number or number not found');
             }
         }
     }
-  const num = numArray.map((a, b) => a + b );
-  return num
+  console.log(numArray);
+  console.log(numArray.reduce((a, b) => a + b ))
+  return numArray.reduce((a, b) => a + b );
 }
 
-
- parseIntReloaded("two thousand two hundred");
+ parseIntReloaded("thirty five hundred");
