7ef4df6a347d1cbd2ca5c51ee1fb63e569e75123
Daniel Epstein <d.epstein92@gmail.com>
2019-03-07 00:00:14 -0500

    added pascalToSnake

---
+++ pascalToSnake.js
@@ -0,0 +1,49 @@
+
+
+/*
+Complete the function/method so that it takes CamelCase string and returns the string in snake_case notation. Lowercase characters can be numbers. If method gets number, it should return string.
+
+Examples:
+
+//  returns test_controller
+toUnderscore('TestController');
+
+// returns movies_and_books
+toUnderscore('MoviesAndBooks');
+
+// returns app7_test
+toUnderscore('App7Test');
+
+// returns "1"
+toUnderscore(1);
+
+*/
+
+
+
+function toUnderscore(string) {
+    if (string.constructor === Number) {
+        return `${string}`;
+    }
+    return string
+        .split("")
+        .map((char, index) => {
+            let digits = '012345679'.split('');
+
+            if (digits.includes(char)) {
+                return char;
+            }
+
+            if (char.toUpperCase() === char) {
+                if (index === 0) {
+                    return char.toLowerCase();
+                } else {
+                    return "_" + char.toLowerCase();
+                }
+            } else {
+                return char;
+            }
+
+        })
+        .join('');
+  }
